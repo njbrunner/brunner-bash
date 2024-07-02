3233cf89ee41cad95d6e88f826d1c5180946c5f3
njbrunner <njbrunner@gmail.com>
2024-07-02 17:13:02 +0000

    Deploying to gh-pages from @ njbrunner/brunner-bash@09421cf2a1e4a4680e2c71847233844c4dcf7f0d 🚀

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,3 +1,5 @@
+let guests = 1;
+
 function submitRSVP() {
   let formValid = validate();
 
@@ -9,6 +11,12 @@ function submitRSVP() {
       attending: document.querySelector('input[name="rsvp-selection"]:checked')
         .value,
     };
+    if (data.attending == "true") {
+      data.preference = document.querySelector(
+        'input[name="accomodation-selection"]:checked'
+      ).value;
+    }
+    console.log(data);
 
     let submitButton = document.getElementById("submitButton");
     submitButton.disabled = true;
@@ -68,4 +76,77 @@ function resetForm() {
   Array.from(forms).forEach((form) => {
     form.classList.remove("was-validated");
   });
+}
+
+function addAccomodationSelection() {
+  let accomodationSelectionSection = document.getElementById(
+    "accomodationSelection"
+  );
+  accomodationSelectionSection.innerHTML = "";
+  if (
+    document.querySelector('input[name="rsvp-selection"]:checked').value ==
+    "true"
+  ) {
+    let campingSelection = document.createElement("div");
+    campingSelection.classList.add("col", "text-end");
+    let cabinSelection = document.createElement("div");
+    cabinSelection.classList.add("col", "text-start");
+    campingSelection.innerHTML = `
+      <input class="btn-check form-check"
+        type="radio"
+        name="accomodation-selection"
+        id="campingInput"
+        autocomplete="off"
+        value="camping"
+        required>
+      <label class="btn btn-colored" for="campingInput">Camping</label>`;
+    cabinSelection.innerHTML = `
+        <input class="btn-check form-check"
+          type="radio"
+          name="accomodation-selection"
+          id="cabinInput"
+          autocomplete="off"
+          value="cabin"
+          required>
+        <label class="btn btn-colored" for="cabinInput">Cabin</label>`;
+    accomodationSelectionSection.appendChild(campingSelection);
+    accomodationSelectionSection.appendChild(cabinSelection);
+  } else {
+    accomodationSelectionSection.innerHTML = "";
+  }
+}
+
+function addGuest() {
+  guests += 1;
+  let guestContainer = document.getElementById("nameContainer");
+  let guestRow = document.createElement("div");
+  guestRow.classList.add("row");
+  guestRow.innerHTML = `
+      <div class="col-sm form-floating mb-3"><input class="form-control needs-validation input-colored"
+          id="firstNameInput${guests}"
+          type="text"
+          name="firstName${guests}"
+          placeholder="First Name"
+          required><label class="ms-3 label-colored"
+          for="firstNameInput${guests}">First Name</label>
+        <div class="invalid-feedback feedback-white">Please provide your first name.</div>
+      </div>
+      <div class="col-sm form-floating mb-3"><input class="form-control needs-validation input-colored"
+          id="lastNameInput${guests}"
+          type="text"
+          name="lastName${guests}"
+          placeholder="Last Name"
+          required><label class="ms-3 label-colored"
+          for="lastNameInput${guests}">Last Name</label>
+        <div class="invalid-feedback feedback-white">Please provide your last name</div>
+      </div>
+      <div class="col-sm-1 form-floating mb-3">
+        <button class="btn btn-outline-light btn-colored form-control py-3"><i
+            class="fa-solid fa-trash"></i></button>
+      </div>`;
+  guestContainer.appendChild(guestRow);
+}
+
+function removeGuest() {
+  
 }
